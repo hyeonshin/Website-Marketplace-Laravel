870678685b17dc1f4921f16097b4cf86e77fabfb
hyeonshin <nashrullah1807@gmail.com>
2026-05-18 14:10:09 +0700

    feat(StoreBalance) : create get all endpoint

---
+++ app/Http/Controllers/StoreBalanceController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ResponseHelper;
+use App\Http\Resources\PaginateResource;
+use App\Http\Resources\StoreBalanceResource;
+use App\Interfaces\StoreBalanceRepositoryInterface;
+use Illuminate\Http\Request;
+
+class StoreBalanceController extends Controller
+{
+    
+    private StoreBalanceRepositoryInterface $storeBalanceRepository;
+
+    public function __construct(StoreBalanceRepositoryInterface $storeBalanceRepository)
+    {
+        $this->storeBalanceRepository = $storeBalanceRepository;
+    }
+
+    public function index(Request $request)
+    {
+        try {
+            $storeBalances = $this->storeBalanceRepository->getAll(
+                $request->search,
+                $request->limit,
+                true
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data Dompet Toko Berhasil Diambil', StoreBalanceResource::collection($storeBalances), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    public function getAllPaginated(Request $request)
+    {
+        $request = $request->validate([
+            'search' => 'nullable|string',
+            'row_per_page' => 'required|integer'
+        ]);
+        
+        try {
+            $storeBalances = $this->storeBalanceRepository->getAllPaginated(
+                $request['search'] ?? null,
+                $request['row_per_page']
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data User Berhasil Diambil', PaginateResource::make($storeBalances, StoreBalanceResource::class), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
